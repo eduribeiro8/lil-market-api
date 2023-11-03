ec38e161915e26dc57ec334c1c877aad73f66164
eduribeiro8 <eduribeiro8@outlook.com>
2023-11-03 00:10:43 -0300

    initial commit

---
+++ src/main/java/com/eduribeiro8/LilMarket/LilMarketApplication.java
@@ -0,0 +1,13 @@
+package com.eduribeiro8.LilMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduribeiro8/LilMarket/LilMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduribeiro8.LilMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
