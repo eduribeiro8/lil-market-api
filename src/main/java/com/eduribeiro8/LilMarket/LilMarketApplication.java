6040b22157de5763e200cbf3c671ad438ca986bc
Eduardo Ribeiro <eduribeiro8@outlook.com>
2026-04-13 21:57:11 -0300

    config: update page JSON serialization format to VIA_DTO

--- src/main/java/com/eduribeiro8/LilMarket/LilMarketApplication.java
+++ src/main/java/com/eduribeiro8/LilMarket/LilMarketApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class LilMarketApplication {
 
 	public static void main(String[] args) {
